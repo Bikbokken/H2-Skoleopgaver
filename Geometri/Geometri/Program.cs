3dad61165300064500d5460dd072fa95e780e96e
Bikbokken <benjaminburek05@gmail.com>
2024-02-27 10:22:12 +0100

    Geometri

---
+++ Geometri/Geometri/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Geometri;
+
+
+List<Figure> figures = new List<Figure>();
+
+
+Rectangle rec = new Rectangle(5, 10);
+Paralellogram par = new Paralellogram(5, 10, 20);
+Trapetz trapetz = new Trapetz(10, 9, 8, 9);
+Triangle triangle = new Triangle(10, 5, 11);
+Square sq = new Square(10);
+
+figures.Add(rec);
+figures.Add(par);
+figures.Add(trapetz);
+figures.Add(triangle);
+figures.Add(sq);
+
+
+figures.ForEach(x => Console.WriteLine(x.ToString()));
---
+++ Geometri/Geometri/Square.cs
@@ -0,0 +1,153 @@
+﻿using Geometri;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Geometri
+{
+    public abstract class Figure
+    {
+        protected double _a = 0;
+
+        public abstract double Areal();
+        public abstract double Perimeter();
+
+        public Figure(double a) {
+            _a = a;
+        }
+
+        public override string ToString()
+        {
+            return $"Areal: {Areal()} - Omkreds: {Perimeter()}";
+        }
+    }
+
+    public class Triangle : Figure
+    {
+
+        protected double _b;
+        protected double _c;
+        public Triangle(double a, double b, double c) : base(a)
+        {
+            _b = b;
+            _c = c;
+        }
+
+        public override double Perimeter()
+        {
+            return _a + _b + _c;
+        }
+
+        public override double Areal()
+        {
+            return 0.5 * _a * _b;
+        }
+    }
+
+    public class Trapetz : Figure
+    {
+        protected double _b;
+        protected double _c;
+        protected double _d;
+        protected double _s;
+
+        protected double _h;
+
+
+        public Trapetz(double a, double b, double c, double d) : base(a)
+        {
+            _b = b;
+            _c = c;
+            _d = d;
+            FindSide();
+            FindHeight();
+        }
+
+        public override double Perimeter()
+        {
+            return _a + _b + _c + _d;
+        }
+
+        private void FindSide()
+        {
+            _s = (_a + _b - _c + _d) / 2;
+        }
+
+        private void FindHeight()
+        {
+            _h = (2/(_a-_c))*Math.Sqrt(_s *(_s-_a+_c)*(_s-_b)*(_s-_d));
+        }
+
+        public override double Areal()
+        {
+            return 0.5 *(_a+_c)*_h;
+        }
+    }
+
+    public class Paralellogram : Figure
+    {
+        protected double _b;
+        protected double _v;
+
+        public Paralellogram(double a, double b, double v) : base(a)
+        {
+            _b = b;
+            _v = v;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * _a + 2 * _b;
+        }
+
+        public override double Areal()
+        {
+            return _a * _b * Math.Sin(_v*(Math.PI)/180);
+        }
+    }
+
+
+
+    public class Rectangle : Paralellogram
+    {
+        private static double angle = 90;
+        public Rectangle(double a, double b) : base(a, b, angle)
+        {
+        }
+
+        public override double Areal()
+        {
+            return base.Areal();
+        }
+
+
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(double a) : base(a, a)
+        {
+        }
+
+        public override double Areal()
+        {
+            return _a * _a;
+        }
+
+        public override double Perimeter()
+        {
+            return _a * 2 + _b * 2;
+        }
+
+
+    }
+
+
+
+
+}
