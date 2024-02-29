4e255d6db8290c5430ec3f1210969f5a8c81fb4c
Bikbokken <benjaminburek05@gmail.com>
2024-02-29 10:45:47 +0100

    BirdsFlyingAround

---
+++ BirdsFlyingAroundApp/BirdsFlyingAroundApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.AccessControl;
+
+public abstract class Bird : IBird
+{
+    public abstract void SetLocation(double longitude, double latitude);
+    public abstract void Draw();
+}
+
+public interface FlyingBird
+{
+     public abstract void SetAltitude(double altitude);
+}
+
+
+
+public interface IBird
+{
+    public abstract void Draw();
+    public abstract void SetLocation(double longitude, double latitude);
+}
+
+public class Duck : Bird, IBird, FlyingBird
+{
+    public override void SetLocation(double longitude, double latitude)
+    {
+        //sæt en lokation
+    }
+    public override void Draw()
+    {
+        //Tegn fuglen på skærmen
+    }
+    public void SetAltitude(double altitude)
+    {
+        // Alltitude
+    }
+}
+
+public class Penguin : Bird, IBird
+{
+    public override void SetLocation(double longitude, double latitude)
+    {
+        //sæt en lokation
+    }
+
+
+    public override void Draw()
+    {
+        //Tegn fuglen på skærmen
+    }
+}
---
+++ HelpLone/HelpLone/Program.cs
@@ -0,0 +1,190 @@
+﻿public interface ICharacter
+{
+    void Heal();
+    void Die();
+    void Fight();
+}
+
+
+public interface CanThrow
+{
+    void ThrowFrostNova();
+    void ThrowMagicMisile();
+
+}
+
+public interface CanStrike
+{
+    void Bash();
+    void Cleave();
+}
+
+public interface CanSlash
+{
+    void Slash();
+}
+
+public interface HasShield
+{
+    void ShieldGlare();
+    void RaiseShield();
+}
+
+
+
+
+public interface CanTeleport
+{
+    void Teleport(int x, int y);
+}
+
+
+public class Wizard : ICharacter, CanTeleport, CanThrow
+{
+    public void Die()
+    {
+        Console.WriteLine("I'm dying");
+    }
+
+    public void Fight()
+    {
+        Console.WriteLine("I'm fighting");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("I'm healing");
+    }
+    public void Teleport(int x, int y)
+    {
+        Console.WriteLine("I'm teleporting to " + x + " " + y);
+    }
+
+    public void ThrowFrostNova()
+    {
+        Console.WriteLine("I'm throwing my frost nova");
+    }
+
+    public void ThrowMagicMisile()
+    {
+        Console.WriteLine("I'm throwing a magic misile");
+    }
+}
+
+
+
+
+public class Babarian : ICharacter, CanSlash, CanStrike
+{
+    public void Bash()
+    {
+        Console.WriteLine("I'm bashing someone");
+    }
+
+    public void Cleave()
+    {
+        Console.WriteLine("I'm cleaving someone");
+    }
+
+    public void Die()
+    {
+        Console.WriteLine("I'm dying");
+    }
+
+    public void Fight()
+    {
+        Console.WriteLine("I'm fighting");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("I'm healing");
+    }
+
+
+    public void Slash()
+    {
+        Console.WriteLine("I'm slashing someone");
+    }
+}
+
+
+
+
+public class Knight : ICharacter, HasShield, CanSlash, CanStrike
+{
+    public void Bash()
+    {
+        Console.WriteLine("I'm bashing someone");
+    }
+
+    public void Cleave()
+    {
+        Console.WriteLine("I'm cleaving someone");
+    }
+
+    public void Die()
+    {
+        Console.WriteLine("I'm dying");
+    }
+
+    public void Fight()
+    {
+        Console.WriteLine("I'm fighting");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("I'm healing");
+    }
+
+    public void RaiseShield()
+    {
+        Console.WriteLine("I'm raising my shield");
+    }
+
+    public void ShieldGlare()
+    {
+        Console.WriteLine("I'm throwing shield glare");
+    }
+
+    public void Slash()
+    {
+        Console.WriteLine("I'm slashing someone");
+    }
+}
+
+
+
+public class Witch : ICharacter, CanTeleport
+{
+    public void Die()
+    {
+        Console.WriteLine("I'm dying");
+    }
+
+    public void Fight()
+    {
+        Console.WriteLine("I'm fighting");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("I'm healing");
+    }
+
+    public void RaiseShield()
+    {
+        Console.WriteLine("I'm raising my shield");
+    }
+
+    public void ShieldGlare()
+    {
+        Console.WriteLine("I'm throwing shield glare");
+    }
+
+    public void Teleport(int x, int y)
+    {
+        Console.WriteLine("I'm teleporting to " + x + " " + y);
+    }
+}
